2a17a226cd135311b44bff838e179a9b19874c53
Amy Gori <amydgori@gmail.com>
2018-05-29 13:31:02 -0400

    Controller fix

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -6,44 +6,25 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get :index
+    get items_url
     assert_response :success
-    assert_not_nil assigns(:items)
   end
 
   test "should get new" do
-    get :new
+    get new_item_url
     assert_response :success
   end
 
   test "should create item" do
     assert_difference('Item.count') do
-      post :create, item: { description: @item.description, image_url: @item.image_url, title: @item.title, starting_bid: @item.starting_bid }
+      post items_path item: {
+        artist: 'Jean Auguste Dominique Ingres',
+        description: 'Oil on canvas',
+        image_url: 'https://images.metmuseum.org/CRDImages/rl/web-large/DT717.jpg',
+        title: 'Princesse de Broglie',
+        starting_bid: '48000'
+      }
     end
-
-    assert_redirected_to item_path(assigns(:item))
-  end
-
-  test "should show item" do
-    get :show, id: @item
     assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @item
-    assert_response :success
-  end
-
-  test "should update item" do
-    patch :update, id: @item, item: { description: @item.description, image_url: @item.image_url, title: @item.title, starting_bid: @item.starting_bid }
-    assert_redirected_to item_path(assigns(:item))
-  end
-
-  test "should destroy item" do
-    assert_difference('Item.count', -1) do
-      delete :destroy, id: @item
-    end
-
-    assert_redirected_to items_path
   end
 end
